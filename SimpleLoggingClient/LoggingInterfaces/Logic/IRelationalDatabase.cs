f7c060751f1b777d71516f5c3bbd48994928190b
Thomas Nelson <thomasvnelson@live.com>
2019-08-28 22:21:16 -0400

    Added Updated SimpleLoggingInterfaces which includes IRelationalDatabaseEntity
    Also added option for encryption.
    Environment Names have been updated

---
+++ SimpleLoggingClient/LoggingInterfaces/Logic/IRelationalDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static SimpleLoggingInterfaces.Enums.EnumCollection;
+
+namespace SimpleLoggingClient.LoggingInterfaces.Logic
+{
+    public interface IRelationalDatabase
+    {
+        void Transaction(LogLevel logLevel, string query, string result, bool writeToPlatform);
+
+        void Transaction(LogLevel logLevel, string query, string result, string note, bool writeToPlatform);
+
+        void Error(LogLevel logLevel, Exception exception, bool innerExceptionOnly, bool writeToPlatform);
+
+        void Error(LogLevel logLevel, Exception exception, string note, bool innerExceptionOnly, bool writeToPlatform);
+
+        IRelationalDatabase PopulateApplicationEntity(LogLevel logLevel, string query, string result, string note, bool writeToPlatform);
+
+        IRelationalDatabase PopulateApplicationEntity(LogLevel logLevel, Exception exception, string note, bool innerExceptionOnly, bool writeToPlatform);
+    }
+}
